923dca61cdaa718ccacbb98431e554a6c7e757f4
Nik Illerbrun <nik.illerbrun@gmail.com>
2014-01-15 11:35:05 -0500

    changed the tokenizer to pass the token struct, output will now print tokens

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -8,13 +8,13 @@
 (define (tokenize char-list state lexeme)
   (cond
     [(empty? char-list) empty]
-    [(not (equal? (get-transition state (first char-list)) STATE_NONE)) (tokenize (rest char-list) (get-transition state (first char-list)) (string-append (make-string 1 (first char-list)) lexeme))]
-    [(not (equal? (get-state-type state) TOKEN_WHITESPACE)) (cons (get-state-type state) (tokenize (rest char-list) (get-transition STATE_START (first char-list)) (make-string 1 (first char-list))))]
+    [(not (equal? (get-transition state (first char-list)) STATE_NONE)) 
+        (tokenize (rest char-list) (get-transition state (first char-list)) (string-append lexeme (make-string 1 (first char-list))))]
+    [(not (equal? (get-state-type state) TOKEN_WHITESPACE)) 
+        (cons (token (get-state-type state) lexeme)
+              (tokenize (rest char-list) (get-transition STATE_START (first char-list)) (make-string 1 (first char-list))))]
     [else (tokenize (rest char-list) (get-transition STATE_START (first char-list)) (make-string 1 (first char-list)))]))
-
-;(token (get-state-type state) lexeme)
 
 (define file "input.txt")
 (define clist (string->list (file->string file)))
-
-(tokenize clist STATE_START "")
+(for-each (lambda (x) (printf "~a: ~a~n" (token-type x) (token-lexeme x))) (tokenize clist STATE_START ""))
