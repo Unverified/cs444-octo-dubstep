11e9c7e6fb139babc82798f0cec4a3bad76cb04b
Nik Illerbrun <nik.illerbrun@gmail.com>
2014-02-01 15:30:29 -0500

    added functions to aid in the creation of regexs

--- token.rkt
+++ token.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require racket/string)
 (provide token-exps)
 
 (define keyword-list '(abstract boolean break byte case catch char class const continue default 
@@ -25,9 +26,24 @@
                    (bool-lit "true|false")
                    (decimal-lit "0|(1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)")
                    (octal-lit "0((0|1|2|3|4|5|6|7)*)")
-                   (hex-lit "0(x|X)((0|1|2|3|4|5|6|7|8|9|a|A|b|B|c|C|d|D|e|E|f|F)*)"))
-		   (floating-point-lit "((0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*).((0|1|2|3|4|5|6|7|8|9)*)(((E|e)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*))|~)(F|f|D|d|~))|((0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(e|E)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(F|f|d|D|~))|(.(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(((E|e)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*))|~)(F|f|D|d|~))|((0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(((E|e)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*))|~)(F|f|D|d))")
-		
+                   (hex-lit "0(x|X)((0|1|2|3|4|5|6|7|8|9|a|A|b|B|c|C|d|D|e|E|f|F)*)")))
+		   ;(floating-point-lit "((0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*).((0|1|2|3|4|5|6|7|8|9)*)(((E|e)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*))|~)(F|f|D|d|~))|((0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(e|E)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(F|f|d|D|~))|(.(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(((E|e)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*))|~)(F|f|D|d|~))|((0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*)(((E|e)(+|-|~)(0|1|2|3|4|5|6|7|8|9)((0|1|2|3|4|5|6|7|8|9)*))|~)(F|f|D|d))")
+
+(define (char-range f l)
+  (map integer->char (range (char->integer f) (add1 (char->integer l)))))
+
+(define (make-union-regex cl)
+  (define (make-escaped-string c)
+    (cond
+      [(not (char? c)) (error "what are you doing, ya dingus?")]
+      [(ormap (curry char=? c) (string->list "()\\|~#")) (string #\\ c)]
+      [else (string c)]))
+  (string-join (map make-escaped-string cl) "|" #:before-first "(" #:after-last ")"))
+
+(define all-ascii (map integer->char (range 0 128)))
+(define java-char (append (string->list "_$") (char-range #\a #\z) (char-range #\A #\Z)))
+(define digits (char-range #\0 #\9))
+
 (define token-exps (append keywords
                            operators
                            separators
