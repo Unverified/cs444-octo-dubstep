6434af4883da5c15204642da36348b224a0043d7
wwchalme <wwchalme@uwaterloo.ca>
2014-02-05 09:37:04 -0500

    Just some parentheses shuffled

--- token.rkt
+++ token.rkt
@@ -34,9 +34,9 @@
                    (floating-point-lit "#(digits).(#(digits)|~)(#(exponent-part)|~)(#(float-type-suffix)|~)|(.#(digits)(#(exponent-part)|~)(#(float-type-suffix)|~))|(#(digits)#(exponent-part)(#(float-type-suffix)|~))|(#(digits)(#(exponent-part)|~)#(float-type-suffix))")
                
 		
-                   (hex-lit "0(x|X)((0|1|2|3|4|5|6|7|8|9|a|A|b|B|c|C|d|D|e|E|f|F)*)")
+                   (hex-lit "0(x|X)((0|1|2|3|4|5|6|7|8|9|a|A|b|B|c|C|d|D|e|E|f|F)*)")))
 
-(define others '((
+(define others '(
 
                    (java-letter "$|_|Q|W|E|R|T|Y|U|I|O|P|A|S|D|F|G|H|J|K|L||Z|X|C|V|B|N|M|q|w|e|r|t|y|u|i|o|p|a|s|d|f|g|h|j|k|l|z|x|c|v|b|n|m")
                    (java-digit "0|1|2|3|4|5|6|7|8|9")
@@ -45,7 +45,7 @@
                    (signed-integer "(+|-|~)#(digits)")
 		   (octal-digit "0|1|2|3|4|5|6|7")
 		   (zero-to-three "0|1|2|3")
-		   (escape-sequence "\\(b|t|n|f|r|\'|\"|\\|(#(octal-digit)|(#(octal-digit)#(octal-digit))|(#(zero-to-three)#(octal-digit)#(octal-digit))))")))))
+		   (escape-sequence "\\(b|t|n|f|r|\'|\"|\\|#(octal-digit)|(#(octal-digit)#(octal-digit))|(#(zero-to-three)#(octal-digit)#(octal-digit)))")))
 (define (char-range f l)
   (map integer->char (range (char->integer f) (add1 (char->integer l)))))
 
