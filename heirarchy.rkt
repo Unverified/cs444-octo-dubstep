0cfbde9d833d6892c6e8a6d532b3815e1b63217b
wwchalme <wwchalme@uwaterloo.ca>
2014-02-28 16:32:26 -0500

    Added tests for cyclic test

--- heirarchy.rkt
+++ heirarchy.rkt
@@ -7,20 +7,29 @@
 
 
 
-;;build-heirarchy : ast -> heirarchy 
+;;build-heirarchy : environment -> heirarchy
+
 
 ;;cyclic? : (id-getter : Y -> X) (children-getter : Y -> listof Y) (visited-nodes : listof X) (node : Y) -> Boolean
 ;;Should be able to detect cycle in any graph given
 
 (define (cyclic? id-getter children-getter visited-nodes node)
   (cond
-    [(empty? (children-getter node)) #t]
-    [(member (id-getter node) visited-nodes) #f]
+    [(member (id-getter node) visited-nodes) #t]
+    [(empty? (children-getter node)) #f]
     [else
-     (let [(id (id-getter node))]
+     (let [(id (id-getter node))
+       
+           (children (children-getter node))]
        (ormap (lambda (child)
                    (cyclic? id-getter children-getter (cons id visited-nodes) child))
-              (children-getter node)))]))
+              children))]))
                  
+;;testing
+(struct simple-graph (id children))
+(cyclic? simple-graph-id simple-graph-children empty (simple-graph 'A (list (simple-graph 'B empty) (simple-graph 'C empty))))
+
+(cyclic? simple-graph-id simple-graph-children empty (simple-graph 'A (list (simple-graph 'B (list (simple-graph 'A empty))) (simple-graph 'C empty))))
+
 
 
