39aeb30328df8c70a6f6abfdc741821ca5318f57
wwchalme <wwchalme@uwaterloo.ca>
2014-03-08 19:18:21 -0500

    Changed some things to make the compiler happy

--- type-expr.rkt
+++ type-expr.rkt
@@ -4,6 +4,8 @@
 (require "environments.rkt")
 (require "heirarchy-checker.rkt")
 
+
+
 (provide type-expr ast)
 
 
@@ -13,16 +15,27 @@
   (error "parent-of not implemented"))
 
 
+;;num-type<? : ptype ptype -> Boolean
+(define (num-type<? pt1 pt2)
+  (define valid-types '(int short char byte long float double))
+  (match (list (ptype-type pt1) (ptype-type pt2))
+    [(list 'byte t) (list? (member t '(short int long float double)))]
+    [(list 'short t) (list? (member t '(int long float double)))]
+    [(list 'char t) (list? (member t '(int long float double)))]
+    [(list 'int t) (list? (member t '(long float double)))]
+    [(list 'long t) (list? (member t '(float double)))]
+    [(list 'float t) (list? member t '(double))]
+    [_ #f]))
 
 ;;can-assign? (union ptype rtype atype) (union ptype rtype atype) -> Boolean
 (define (can-assign? T S)
   (match (list T S)
     [(list (rtype _ _) (ptype _ _)) (error "Assignment of primitive types to reference type variables not allowed")]
-    
+    [(list (rtype _ _) (ptype _ 'null)) #t]
+    [_ (error "Not Implemented")]))
     
 
     
-  (error "can-assign? not implemented"))
 
 ;;cast-ptypes : Symbol Symbol -> Boolean
 (define (cast-ptypes T S)
@@ -34,7 +47,7 @@
   (match (list T S)
     [(list (ptype _ sym1) (ptype _ sym2)) (cast-ptypes sym1 sym2)]
     [(list (atype _ typ1) (atype _ typ2)) (begin (printf "Warning: I'm not sure how to properly cast array types") (castable? typ1 typ2))]
-    [(list (rtype _ _) (rtype _ _)) (if (type-ast=? T S) #t (or (parent-of? T S env) (parent-of T S env)))]
+    [(list (rtype _ _) (rtype _ _)) (if (type-ast=? T S) #t (or (parent-of? T S env) (parent-of? S T env)))]
     [(list _ _) (error "Cast type mismatch")]))
 
 
@@ -85,7 +98,7 @@
     
     [(literal _ type value) type]
     [(or
-      (ptype _ _ _) (atype _ _ _) (rtype _ _ _)) ast]
+      (ptype _ _) (atype _ _ ) (rtype  _ _)) ast]
     
     
     [(cast e c expr) 
@@ -119,22 +132,21 @@
     [(arraycreate _ type size) (begin (type-expr type) (if (whole-number? (type-expr size)) 
                                                         (atype type)   
                                                         (error "Array declaration expects numeric type for size")))]
-    [(methodcall _ left args) 
+    [(methodcall _ _ _ args) 
      (let* ([method-funt (methodcall->funt ast type-expr)]
             [ret (match (assoc  method-funt (envs-types env))
                       [(list a b) b]
                       [_ (error "No Function of that name")])])
        ret)]
            
-           ]
     [(methoddecl _ id parameters) (error "Attempt to type Method Declaration")]
-    [(method _ _ _ _ body) (type-expr body)]
+    [(method _ _ _ _ _ body) (type-expr body)]
     [(or (class _ _ _ _ _ _ body)
          (interface _ _ _ _ _ body)) (type-expr body)]
     [(cunit _ _ body) (type-expr body)]
     
     [(fieldaccess _ _ field) 
-     (match (assoc id (envs-types env))
+     (match (assoc field (envs-types env))
        [#f (error "Unbound Field Access")]
        [(list a b) b])]
     
