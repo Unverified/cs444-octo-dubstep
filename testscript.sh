9425eb8536bab3fdcd85baeed4ef0fb3faaae29d
Nick F <nich.fagan@gmail.com>
2014-03-12 20:21:06 -0400

    Changed the testscript so we can run tests on subfolders in tests/in/a[1..5], read the comments at the top of testscript.sh if you want to know how to use it

--- testscript.sh
+++ testscript.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 
-# Usage: bash testscript.sh [-a] [regex]
-# -a : do not stop the testscript if it encounters an error, if this flag is not included then the script will terminated on the first error it encounters
-# regex : a regular expression, so you can specify what files to run the test script on in tests/in (eg "bash testscript a*" will run all files in tests/in/ beginning with a). If no regex is supplied then all files in tests/in/ are run.
+# Usage: bash testscript.sh [-s] [a# [files in a#] ]
 
-# The script will run the compiler on all the files found in tests/in/ directory and compare the output with the contents of the files found in tests/out/ with the same name. If it finds a difference then it will re-run the compiler with the debug flag set and place the input in tests/failed_out/
+# -s stops the compiler when a test fails
+# a# tells the script what subfolder to look in for tests (ie a1, a2, a3, a4, a5), defaults to a3
+# "files in a#" tells the script what files in a# to run 
+
+# example: bash testscript.sh a2 "J1*" -> will run all the tests in tests/in/a1/ starting with J1
 
 TEST_DIR="tests"
 
 STDLIB=`find stdlib -iname "*.java"`
-IN_TEST_DIR="$TEST_DIR/in/a2"
-OUT_TEST_DIR="$TEST_DIR/out/a2"
+IN_TEST_DIR="$TEST_DIR/in"
+OUT_TEST_DIR="$TEST_DIR/out"
 FAILED_TEST_DIR="$TEST_DIR/failed_out"
 
-# get the command-line arguments
-files_to_run="$IN_TEST_DIR/*"
-flag=""
+A="a3"
+files_to_run="*"
+stop_on_fail=false
 
 if [ "$1" != "" ]
 then
-  if [[ "$1" == -* ]]
+  if [[ "$1" == -s ]]
   then
-    flag="$1"
+    stop_on_fail=true
+    if [ "$2" != "" ]
+    then
+      A="$2"
+    fi
+
+    if [ "$3" != "" ]
+    then
+      files_to_run="$3"
+    fi
   else
-    files_to_run="$1"
+    A="$1"
+
+    if [ "$2" != "" ]
+    then
+      files_to_run="$2"
+    fi
   fi
 fi
 
-if [ "$2" != "" ]
-then
-  files_to_run="$2"
-fi
 
 make
+mkdir -p $FAILED_TEST_DIR/$A/
 
 echo ""
-echo "Using input files located in: \"$IN_TEST_DIR/\""
-echo "Using output files located in \"$OUT_TEST_DIR/\""
-echo "Running tests on: $files_to_run"
+echo "Using input files located in: \"$IN_TEST_DIR/$A/\""
+echo "Using output files located in \"$OUT_TEST_DIR/$A/\""
+echo "Running tests on: $IN_TEST_DIR/$A/$files_to_run"
 echo ""
 
-for test in $files_to_run
+for test in $IN_TEST_DIR/$A/$files_to_run
 do
   if [ -d "$test" ]				
   then
@@ -55,25 +68,26 @@ do
   dir_array=(`echo $test | tr '/' ' '`)
   let test_name_pos=${#dir_array[@]}-1
   test_name=${dir_array[$test_name_pos]}
-  out_file=$OUT_TEST_DIR/$test_name
+  out_file=$OUT_TEST_DIR/$A/$test_name
 
   echo "Running compiler with files:"
   echo "$test_files"
   echo "$STDLIB"
     
-  racket compiler.rkt $test_files  > $FAILED_TEST_DIR/$test_name.out
+  racket compiler.rkt $test_files > $FAILED_TEST_DIR/temp.out
   test_output=$?
   expected_output=`cat $out_file`
 
   if [ "$test_output" == "$expected_output" ]
   then
-    rm $FAILED_TEST_DIR/$test_name.out
+    rm $FAILED_TEST_DIR/temp.out
     echo "Test Passed!"
   else
+    mv $FAILED_TEST_DIR/temp.out $FAILED_TEST_DIR/$A/$test_name
     echo "Test Failed! Compiler returned: $test_output, Expected: $expected_output"
-    echo "Output in file: $FAILED_TEST_DIR/$in_file_name.out"
+    echo "Output in file: $FAILED_TEST_DIR/$A/$test_name"
 
-    if [ "$flag" != "-a" ]
+    if [ "$stop_on_fail" == "true" ]
     then
       exit
     fi
