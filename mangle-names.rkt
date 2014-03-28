f4dde41a68187ede148ec42196983e315c15af40
wwchalme <wwchalme@uwaterloo.ca>
2014-03-28 16:52:48 -0400

    x x

--- mangle-names.rkt
+++ mangle-names.rkt
@@ -8,11 +8,21 @@
 ;;mangling names
 
 
-(define (mangle-names funt)
-  (string-append (funt-id funt) (foldr string-append "" (map get-mangled-type-name (funt-argt funt)))))
+(define (mangle-names thing)
+	(match thing
+		[(funt id params) (string-append id (foldr string-append "" (map get-mangled-type-name params))))]
+		[(list cname scope-id var-name) (string-append cname "_" scope-id "_" var-name "_" "var")]))
 
 (define (get-mangled-type-name ast)
   (match ast
     [(ptype name) (string-append "_" (symbol->string name))]
     [(rtype name) (string-append "_r_" (foldr string-append "" name))]
     [(atype t) (string-append "_a_" (get-mangled-type-name t))]))
+
+
+;;class name
+;;scope id 
+;;variable name
+
+
+
