dffbfbb89023d1271c4f6fd1c7984a716be779f2
wwchalme <wwchalme@uwaterloo.ca>
2014-03-31 19:12:22 -0400

    Got rid of merge text

--- assemble-load.sh
+++ assemble-load.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-<<<<<<< HEAD
 for a in output/*.o; do
 	rm ${a}
 done
-=======
-rm -f output/*.o
->>>>>>> 939c92cd12d7988163713f6e07bcaf9670f84739
 cp stdlib/runtime.s output/
 
 for a in output/*.s; do
